5643f905c23a866419f784f0beac88636e5f6e2c
handymandev12 <ejasvith@gmail.com>
2026-02-08 18:49:04 +0000

    correcting logo file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -78,7 +78,7 @@ import TechnicianViewBookTechnician from './TechnicianViewBookTechnician.js';
 import BookTechnicianDetailsNotifications from './BookTechnicianDetailsNotifications.js';
 import BookTechnicianDetailsGrid from './BookTechnicianDetailsGrid.js';
 import RaiseTicketConfirmation from './RaiseTicketConfirmation.js';
-import HandyManLogo from './HandyManLogo.js';
+import HandyManLogo from './HandymanLogo.js';
 import LoginPage from './LoginPage.js';
 import UserIdLogin from './UserIdLogin.js';
 import CustomerRaiseTicketGridView from './CustomerRaiseTicketGridView.js';
